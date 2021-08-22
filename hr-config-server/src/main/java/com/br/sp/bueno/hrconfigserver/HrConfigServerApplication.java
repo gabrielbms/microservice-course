8c99f95a00dca3fc9108376cb9a2807015c3951a
gabrielbms <61248933+gabrielbms@users.noreply.github.com>
2021-08-22 10:18:46 -0300

    Project hr-config-server created

---
+++ hr-config-server/src/main/java/com/br/sp/bueno/hrconfigserver/HrConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.br.sp.bueno.hrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+}
