4afb462bb646b679ae933ef7aeb966b3ba735b38
gabrielbms <61248933+gabrielbms@users.noreply.github.com>
2021-09-04 20:15:21 -0300

    Login and JWT Token

--- hr-oauth/src/main/java/com/br/sp/bueno/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/br/sp/bueno/hroauth/services/UserService.java
@@ -2,6 +2,9 @@ package com.br.sp.bueno.hroauth.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +12,7 @@ import com.br.sp.bueno.hroauth.entities.User;
 import com.br.sp.bueno.hroauth.feighclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -23,6 +26,17 @@ public class UserService {
 			throw new IllegalArgumentException("Email not found");
 		}
 		logger.info("Email found: " + email);
+		return user;
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("Email found: " + username);
 		return user;
 	}
 }
